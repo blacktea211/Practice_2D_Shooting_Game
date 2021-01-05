4e0859018643a658b96698d2e8541eb6a3ffd6d6
blacktea211 <70305824+blacktea211@users.noreply.github.com>
2021-01-05 17:10:08 +0800

    如何受到傷害扣血量

--- UnityProject/Assets/Scripts/Black.cs
+++ UnityProject/Assets/Scripts/Black.cs
@@ -4,6 +4,39 @@ using UnityEngine.UI;
 public class Black : MonoBehaviour
 {
     #region 練習區域 - 在此區域內練習
-    
-    #endregion
+
+    [Header("血量")]
+    public int hp = 10;
+    [Header("血量介面")]
+    public GameObject text_hp;
+    [Header("音效")]
+    public AudioClip clip;
+
+
+
+   
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        hp -= 1;        
+        
+       if (hp<=0)
+       {
+            Die();
+            Destroy(text_hp);
+        }
+       
+           }
+    private void Die() 
+    {
+        Destroy(this.gameObject);
+    }
+    private void Update()
+    {
+        
+         
+    }
 }
+   
+     #endregion
+        
--- UnityProject/Assets/Scripts/White.cs
+++ UnityProject/Assets/Scripts/White.cs
@@ -3,8 +3,35 @@
 public class White : MonoBehaviour
 {
     #region 練習區域 - 在此區域內練習
-    
+
+    [Header("子彈")]
+    public Bullet bullet;
+    [Header("子彈發射位置")]
+    public Transform point;
+    [Header("音效")]
+    public AudioClip clip;
+
+   
+
+    private void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0)) 
+        {
+            Shoot();
+            Instantiate(bullet, point.position, Quaternion.identity);
+           
+        }
+        
+    }
     #endregion
+
+
+    private void Shoot()
+    {
+    }
+
+
 
     #region KID 區域 - 不要偷看 @3@
     [Header("移動速度"), Range(0f, 100f)]
